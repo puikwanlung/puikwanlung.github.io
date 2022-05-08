c2b8f796e2d1194d5d7b828bcf11645c88f09e63
eirineko <eirineko@gmail.com>
2022-05-08 15:13:28 -0400

    update

--- assets/js/filter-portfolio.js
+++ assets/js/filter-portfolio.js
@@ -1,14 +1,14 @@
 const filter_work = document.querySelector("#work-select");
 const filter_UXUI = document.querySelector("#select-UXUI");
 const filter_AE = document.querySelector("#select-AE");
-const filter_PS = document.querySelector("#select-PS");
-const filter_AI = document.querySelector("#select-AI");
+// const filter_PS = document.querySelector("#select-PS");
+const filter_3D = document.querySelector("#select-threeD");
 const filter_all = document.querySelector("#select-all");
 
 const show_UXUI = document.querySelectorAll(".card_UXUI");
 const show_AE = document.querySelectorAll(".card_AE");
-const show_PS = document.querySelectorAll(".card_PS");
-const show_AI = document.querySelectorAll(".card_AI");
+// const show_PS = document.querySelectorAll(".card_PS");
+const show_3D = document.querySelectorAll(".card_threeD");
 
 //Update results when change the select
 filter_all.addEventListener("click", function () {
@@ -20,12 +20,12 @@ filter_all.addEventListener("click", function () {
     show_AE[i].classList.remove("hidden");
   }
 
-  for (var i = 0; i < show_PS.length; i++) {
-    show_PS[i].classList.remove("hidden");
-  }
+  // for (var i = 0; i < show_PS.length; i++) {
+  //   show_PS[i].classList.remove("hidden");
+  // }
 
-  for (var i = 0; i < show_AI.length; i++) {
-    show_AI[i].classList.remove("hidden");
+  for (var i = 0; i < show_3D.length; i++) {
+    show_3D[i].classList.remove("hidden");
   }
 });
 
@@ -38,12 +38,12 @@ filter_UXUI.addEventListener("click", function () {
     show_AE[i].classList.add("hidden");
   }
 
-  for (var i = 0; i < show_PS.length; i++) {
-    show_PS[i].classList.add("hidden");
-  }
+  // for (var i = 0; i < show_PS.length; i++) {
+  //   show_PS[i].classList.add("hidden");
+  // }
 
-  for (var i = 0; i < show_AI.length; i++) {
-    show_AI[i].classList.add("hidden");
+  for (var i = 0; i < show_3D.length; i++) {
+    show_3D[i].classList.add("hidden");
   }
 });
 
@@ -56,16 +56,34 @@ filter_AE.addEventListener("click", function () {
     show_AE[i].classList.remove("hidden");
   }
 
-  for (var i = 0; i < show_PS.length; i++) {
-    show_PS[i].classList.add("hidden");
-  }
+  // for (var i = 0; i < show_PS.length; i++) {
+  //   show_PS[i].classList.add("hidden");
+  // }
 
-  for (var i = 0; i < show_AI.length; i++) {
-    show_AI[i].classList.add("hidden");
+  for (var i = 0; i < show_3D.length; i++) {
+    show_3D[i].classList.add("hidden");
   }
 });
 
-filter_PS.addEventListener("click", function () {
+// filter_PS.addEventListener("click", function () {
+//   for (var i = 0; i < show_UXUI.length; i++) {
+//     show_UXUI[i].classList.add("hidden");
+//   }
+
+//   for (var i = 0; i < show_AE.length; i++) {
+//     show_AE[i].classList.add("hidden");
+//   }
+
+//   for (var i = 0; i < show_PS.length; i++) {
+//     show_PS[i].classList.remove("hidden");
+//   }
+
+//   for (var i = 0; i < show_3D.length; i++) {
+//     show_3D[i].classList.add("hidden");
+//   }
+// });
+
+filter_3D.addEventListener("click", function () {
   for (var i = 0; i < show_UXUI.length; i++) {
     show_UXUI[i].classList.add("hidden");
   }
@@ -74,29 +92,11 @@ filter_PS.addEventListener("click", function () {
     show_AE[i].classList.add("hidden");
   }
 
-  for (var i = 0; i < show_PS.length; i++) {
-    show_PS[i].classList.remove("hidden");
-  }
+  // for (var i = 0; i < show_PS.length; i++) {
+  //   show_PS[i].classList.add("hidden");
+  // }
 
-  for (var i = 0; i < show_AI.length; i++) {
-    show_AI[i].classList.add("hidden");
-  }
-});
-
-filter_AI.addEventListener("click", function () {
-  for (var i = 0; i < show_UXUI.length; i++) {
-    show_UXUI[i].classList.add("hidden");
-  }
-
-  for (var i = 0; i < show_AE.length; i++) {
-    show_AE[i].classList.add("hidden");
-  }
-
-  for (var i = 0; i < show_PS.length; i++) {
-    show_PS[i].classList.add("hidden");
-  }
-
-  for (var i = 0; i < show_AI.length; i++) {
-    show_AI[i].classList.remove("hidden");
+  for (var i = 0; i < show_3D.length; i++) {
+    show_3D[i].classList.remove("hidden");
   }
 });
