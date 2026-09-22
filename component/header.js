3c38631d5d9b5c16f451d5b178fa147ba7a1c7d0
eirineko <92543305+eirineko@users.noreply.github.com>
2026-09-22 16:13:29 -0400

    Update header.js

--- component/header.js
+++ component/header.js
@@ -5,7 +5,7 @@ template.innerHTML = `
 <div id="header-width">
 
 <a href="/">
-  <img src="/assets/images/logo-puikwanlung.gif" id="logo" alt="Logo of Pui Kwan Lung, Program Coordinator & Content Specialist" />
+  <img src="/assets/images/logo-puikwanlung.gif" id="logo" alt="Logo of Pui Kwan Lung, Program & Student Engagement Coordinator" />
 </a>
 <ul class="nav">
   <li class="nav-item">
