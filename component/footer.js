a9ab07718979babc1fdd4ba47490c7681de40b7e
eirineko <92543305+eirineko@users.noreply.github.com>
2026-09-22 16:09:51 -0400

    Update footer.js

--- component/footer.js
+++ component/footer.js
@@ -1,12 +1,10 @@
 const footer = document.createElement("template");
 
-template.innerHTML = `
-<footer><img src="/assets/images/pui_slogo.png"  alt="Logo of Pui Kwan Lung, Interactive Media Designer" class="mb-2 logo" />
-<p>@ 2022 Design & Build by Pui Kwan Lung</p>
-
+template.innerHTML =`
+<footer><img src="/assets/images/pui_slogo.png"  alt="Logo of Pui Kwan Lung, Program Coordinator & Content Specialist" class="mb-2 logo" />
+<p>&copy; <span id="year"></span> Portfolio & Site by Pui Kwan Lung</p>
 
 <ul id="social-links">
-
   <li>
     <img src="/assets/images/icon-linkedin.png" /><a href="https://www.linkedin.com/in/puikwanlung" target="_blank">https://www.linkedin.com/in/puikwanlung</a>
   </li>
@@ -18,3 +16,6 @@ template.innerHTML = `
 `;
 
 document.body.appendChild(template.content);
+
+// Automatically update the footer year
+document.getElementById("year").textContent = new Date().getFullYear();
